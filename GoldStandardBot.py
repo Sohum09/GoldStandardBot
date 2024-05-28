ce14fdebed7cdf5c51a2a319eeb54e61b863890a
Sohum09 <64685057+Sohum09@users.noreply.github.com>
2024-05-28 16:09:20 +0530

    Update GoldStandardBot.py

--- GoldStandardBot.py
+++ GoldStandardBot.py
@@ -999,7 +999,46 @@ async def digty(ctx, name:str, yr:str):
     url = f"http://agora.ex.nii.ac.jp/digital-typhoon/summary/wnp/s/{jma}.html.en"
     await ctx.send(url)
     
+@bot.command(name='digty_image')
+async def digty_image(ctx, name:str, yr:str, hour:str, day:str,  month:str, imageYear:str, enhancement:str, satellite:str):
+    name = name.upper()
+    month = month.zfill(2)
+    day = day.zfill(2)
+    hour = hour.zfill(2)
+    if enhancement == 'BD' or enhancement == 'NHC':
+        enhancement = enhancement.lower()
+    elif enhancement == 'ir1-ir2' or enhancement == 'ir4-ir1':
+        enhancement = enhancement.upper()
+    else:
+        enhancement = enhancement.upper()
+    satellite = satellite.upper()
 
+    satelliteMap = {'GMS1':'GMS1', 'GMS2':'GMS2', 'GMS3':'GMS3', 'GMS4':'GMS4', 'GMS5':'GMS5', 'GOE9':'GOE9', 'MTS1':'MTS1', 'MTS2':'MTS2', 'HMW8':'HMW8', 'HMW9':'HMW9'}
+    enhancementMap = {'VIS':'0', 'IR1':'1', 'IR2':'2', 'IR3': '3', 'IR4': '4', 'BD': 'bd', 'bd': 'bd', 'nhc': 'nhc', 'NHC':'nhc', 'IR1-IR2': 'IR1-IR2', 'IR4-IR1': 'IR4-IR1'}
+
+    if(name == 'NOT_NAMED'):
+        await ctx.send("Due to the IBTRACS database being ambiguous with this name, it cannot be used.")
+        return
+    jma = ""
+    with open("bst_all.txt", "r") as file:
+        data = file.read()
+        data = data.split('\n')
+        for i in range(len(data)-1):
+            row = data[i].split()
+            
+            if row[0] == '66666':
+                
+                if len(row) == 7:
+                    continue
+                if (row[7] == name or row[6] == name) and yr[2:] == row[1][:2]:
+                    print(row)
+                    print(len(row))
+                    jma += yr + row[1][2:]
+                    break
+    print(jma)
+
+    url = f"http://agora.ex.nii.ac.jp/digital-typhoon/wnp/by-name/{jma}/{enhancementMap[enhancement]}/512x512/{satelliteMap[satellite]}{imageYear[2:]}{month}{day}{hour}.{jma}.jpg"
+    await ctx.send(url)
 
 @bot.command(name='ibtracs')
 async def ibtracs(ctx, btkID:str, yr:str):
@@ -4208,6 +4247,14 @@ async def rhoades(ctx):
 
 @bot.command(name='chappal')
 async def chappal(ctx):
+    image_path1 = 'chappal.webp'
+
+    with open(image_path1, 'rb') as image_file:
+        image = discord.File(image_file)
+        await ctx.send(file=image)
+
+@bot.command(name='megaslop')
+async def megaslop(ctx):
     image_path1 = 'chappal.webp'
 
     with open(image_path1, 'rb') as image_file:
